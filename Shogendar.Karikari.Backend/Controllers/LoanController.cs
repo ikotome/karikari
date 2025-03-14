46b2afa2713d009682a42b68ee963163f7342752
ikotome <kotomizu.0728@gmail.com>
2025-03-14 20:12:04 +0900

    ローンcreate,update機能を追加し、Loanモデルを修正。FriendControllerを新規作成。

--- Shogendar.Karikari.Backend/Controllers/LoanController.cs
+++ Shogendar.Karikari.Backend/Controllers/LoanController.cs
@@ -1,6 +1,8 @@
+using System.Xml.XPath;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Shogendar.Karikari.Models;
 
 namespace Shogendar.Karikari.Backend.Controllers
 {
@@ -17,6 +19,29 @@ namespace Shogendar.Karikari.Backend.Controllers
             if(!query.Any())
                 return NotFound();
             return Ok(query.ToList());
+        }
+
+        [HttpPut]
+        public IActionResult CreateLoans(int loanId, Event eventId, int payerId, int repayerId, decimal amount){
+            Db db = new();
+            Loan result;
+            if (loanId == 0){
+                result = new Models.Loan { PayerId = payerId, RepayerId = repayerId, Amount = amount};
+                db.Loans.Add(result);
+            }else{
+                // Loanから loanIdを元に
+                var query = from Loan in db.Loans
+                            where Loan.Id == loanId
+                            select Loan;
+                result = query.FirstOrDefault();
+                if(result is null)
+                    result = new Loan { Id = loanId, Event = eventId };
+                result.PayerId = payerId;
+                result.RepayerId = repayerId;
+                result.Amount = amount;
+            }
+            db.SaveChanges();
+            return Created("", result);
         }
     }
 }
--- Shogendar.Karikari.Models/Loan.cs
+++ Shogendar.Karikari.Models/Loan.cs
@@ -6,7 +6,8 @@ namespace Shogendar.Karikari.Models
     public class Loan
     {
         public int Id { get; set; }
-        public required int Event { get; set; }
+        public int EventId { get; set; }
+        public Event Event { get; set; }
         public int PayerId { get; set; }
         public User Payer { get; set; }
         public int RepayerId { get; set; }
